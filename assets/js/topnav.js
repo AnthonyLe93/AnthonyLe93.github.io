c66291cad3fa7553e722871292ab68c44c70c222
Anthony Le <anthony.le@bristol.ac.uk>
2023-01-04 11:14:57 +0000

    Update the topnav bar design in all html files, got rid of the non-functional search bar, edit the main.css file and add the topnav.css file.

--- assets/js/topnav.js
+++ assets/js/topnav.js
@@ -1,8 +1,8 @@
-(function ($) {
+function myFunction() {
   var x = document.getElementById("myTopnav");
   if (x.className === "topnav") {
     x.className += " responsive";
   } else {
     x.className = "topnav";
   }
-})(jQuery);
+}
